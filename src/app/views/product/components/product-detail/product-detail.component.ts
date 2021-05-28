1169c76483c8ad3f93cb320e020e08e9df0f9b52
Oscar Romero <oromeror@unbosque.edu.co>
2021-05-27 19:35:59 -0500

    Car price stock fix and product image fix

--- src/app/views/product/components/product-detail/product-detail.component.ts
+++ src/app/views/product/components/product-detail/product-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import {ActivatedRoute, Event, Params, Router} from '@angular/router';
+import { ActivatedRoute, Event, Params, Router } from '@angular/router';
 import { CartService } from 'src/app/core/services/cart.service';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProductBuyerDetailService } from 'src/app/core/services/productBuyerDetail/product-buyer-detail.service';
 import * as events from 'events';
 
@@ -16,7 +16,6 @@ export class ProductDetailComponent implements OnInit {
   @Input() product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
-  form: FormGroup;
   prod: any = [];
   id: number;
   selectOptions: string;
@@ -27,35 +26,29 @@ export class ProductDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private buyerDetailService: ProductBuyerDetailService,
     private cartService: CartService,
-    private  formBuilder: FormBuilder,
+    private formBuilder: FormBuilder,
   ) {
-/*    this.buildForm();*/
     this.buyerDetailService.detail$.subscribe(products => {
       this.prod = products[0];
     });
   }
+
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = params.id;
     });
     console.log(this.stock);
-/*    for (const conf of this.prod.configurations) {
+    for (const conf of this.prod.configurations) {
       this.config.push(conf.name);
       this.subconfiguration.push(conf.sub_configuration);
-    }*/
+    }
     console.log('CC - ' + this.subconfiguration);
   }
-/*  updateStok(event: Event) {
-    const value = this.form.value;
-    this.stock = value.stock;
-  }
-  private buildForm() {
-    this.form = this.formBuilder.group({stock: ['', [Validators.required]]});
-  }*/
   addCart() {
-    console.log('añadir al carrito' + this.stock);
-    this.cartService.addCart(this.prod);
+    let stock = (document.getElementById("stock") as HTMLInputElement).value;
+    this.stock = stock;
+    console.log(this.stock)
   }
 
 }
