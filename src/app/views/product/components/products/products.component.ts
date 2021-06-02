7549d2dae3227a132da6762195a9350781ac4878
Santiago <scamachomo@unbosque.edu.co>
2021-06-01 22:41:52 -0500

    Aplicación de filtro para productos

--- src/app/views/product/components/products/products.component.ts
+++ src/app/views/product/components/products/products.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { MatTableDataSource } from '@angular/material';
 import { ProductsService } from '../../../../core/services/products/products.service';
+
 
 
 
@@ -15,38 +16,55 @@ import { ProductsService } from '../../../../core/services/products/products.ser
 export class ProductsComponent implements OnInit {
 
   activeCategories: any[] = [];
+  tempCategories: any[] = [];
   public columns = ['name', 'description', 'price'];
+
+
 
   constructor(
     private productService: ProductsService,
     private router: Router,
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.fetchCategories();
+    this.activeCategories = this.fetchCategories();
     console.log(this.activeCategories);
+
+    this.tempCategories =  this.fetchCategories();
+    console.log(this.tempCategories);
+
+
   }
 
-  // applyFilter(event: Event) {
-  //   const filterValue = (event.target as HTMLInputElement).value;
-  //   this.dataSource.filter = filterValue.trim().toLowerCase();
 
-  //   if (this.dataSource.paginator) {
-  //     this.dataSource.paginator.firstPage();
-  //   }
-  // }
+  applyFilter(x: String) {
+
+    console.log(x);
+    if(x == ''){
+      console.log(this.tempCategories);
+      this.tempCategories = this.fetchCategories();
+     
+    }else{
+      for(const categoryIndex in this.activeCategories){
+                this.tempCategories[categoryIndex].products = this.activeCategories[categoryIndex].products.filter((product) => product.name.toLowerCase().includes(x.toLowerCase()));
+        } 
+    }
+  }
 
   clickProduct(id: number) {
     console.log('product');
     console.log(id);
   }
-
-  fetchCategories() {
+ 
+  fetchCategories(): any[] {
+    const categories: any[]=[];
     this.productService.getEnabledProducts().subscribe((response: any) => {
       for (const category of response.categories) {
-        this.activeCategories.push(category);
+        categories.push(category);
       }
     });
+
+    return categories;
   }
 
   navigateToForm() {
